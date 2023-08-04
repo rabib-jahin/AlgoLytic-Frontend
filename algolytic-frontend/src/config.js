0c5a71f3ef56082a260a6e10c33527f27bebc206
mehrab-haque <mehrab.haque.0001@gmail.com>
2023-08-04 21:42:10 +0600

    done

--- algolytic-frontend/src/config.js
+++ algolytic-frontend/src/config.js
@@ -1,3 +1,4 @@
 // module.exports.devApi='http://localhost:5001'//
 module.exports.devApi='http://43.224.110.132/api/v1.0.0'
-module.exports.prodApi='https://ummg929j9h.execute-api.ap-south-1.amazonaws.com/prod'
+module.exports.prod='http://43.224.110.132/api/v1.0.0'
+//module.exports.prodApi='https://ummg929j9h.execute-api.ap-south-1.amazonaws.com/prod'
